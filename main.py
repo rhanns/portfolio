5e265a7603897ed5bccf3b2721362d57bd2f772c
rhannska <rhannska@users.noreply.github.com>
2024-02-28 17:02:45 -0600

    finished part 1/3 web app video

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ with col2:
     different projects that I have completed so far.
     """
     st.info(content)
+
+content_about = """Below you can find the projects that I have worked on during the course:
+Learn Python completely in 60 days or less by building 20 real-world applications from web development to data science.
+"""
+st.write(content_about)
