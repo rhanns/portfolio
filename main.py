dc1158676f08ddb9ad53f7a8e00196116e9bf90b
rhannska <rhannska@users.noreply.github.com>
2024-02-28 18:25:10 -0600

    day 2/3 final touches.

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ import pandas
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
-
 with col1:
     st.image("images/photo.jpg")
-
 with col2:
     st.title("Ezra Smith")
     content = """
@@ -23,7 +21,6 @@ st.write(content_about)
 
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv("data.csv", sep=";")
-
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
